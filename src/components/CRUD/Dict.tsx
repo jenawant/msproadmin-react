4bb75042aac0bed16498cc97d8b37c942e56bca3
Jena <jena.want@qq.com>
2023-12-13 11:08:54 +0800

    Update

--- src/components/CRUD/Dict.tsx
+++ src/components/CRUD/Dict.tsx
@@ -1,8 +1,12 @@
-import common from '@/services/api/common';
-import { ProFormRadio, ProFormSelect } from '@ant-design/pro-components';
-import React, { useEffect, useState } from 'react';
+import common from "@/services/api/common";
+import { ProFormRadio, ProFormSelect } from "@ant-design/pro-components";
+import React, { useEffect, useState } from "react";
 
-export default (props: { initialValue?: any; fieldProps?: any; formItemProps?: any }) => {
+export default (props: {
+  initialValue?: any;
+  fieldProps?: any;
+  formItemProps?: any;
+}) => {
   const [options, setOptions] = useState<any[]>([]);
   useEffect(() => {
     const fetch = async (params: any) => {
@@ -19,23 +23,31 @@ export default (props: { initialValue?: any; fieldProps?: any; formItemProps?: a
     };
   }, [props.fieldProps.dict, props?.fieldProps?.options]);
   return props.initialValue ? (
-    options?.find((item: any) => item.key === props.initialValue.toString?.())?.title ?? null
-  ) : props.fieldProps.type === 'select' ? (
+    options?.find((item: any) => item.key === props.initialValue.toString?.())
+      ?.title ?? null
+  ) : props.fieldProps.type === "select" ? (
     <ProFormSelect
       name={props.fieldProps.id}
       params={{ ...options }}
       request={async () =>
-        options.map((item: any) => ({
-          label: item[props.fieldProps.fieldNames.label],
-          value: item[props.fieldProps.fieldNames.value],
-        }))
+        options
+          .map((item: any) => ({
+            label: item[props.fieldProps.fieldNames.label],
+            value: item[props.fieldProps.fieldNames.value],
+          }))
+          .map((item: any) => ({
+            label: item.label,
+            value: /^\d+$/.test(item.value) ? Number(item.value) : item.value, //数字字符串转为数字
+          }))
       }
       showSearch={props.fieldProps.showSearch}
-      mode={props.fieldProps.multiple ? 'multiple' : 'single'}
+      mode={props.fieldProps.multiple ? "multiple" : "single"}
       fieldProps={{
-        optionFilterProp: 'children',
+        optionFilterProp: "children",
         filterOption: (input: string, option: any) =>
-          (option?.searchLabel ?? option?.label ?? '').toLowerCase().includes(input.toLowerCase()),
+          (option?.searchLabel ?? option?.label ?? "")
+            .toLowerCase()
+            .includes(input.toLowerCase()),
         allowClear: true,
       }}
       {...props.formItemProps}
@@ -46,10 +58,15 @@ export default (props: { initialValue?: any; fieldProps?: any; formItemProps?: a
       name={props.fieldProps.id}
       params={{ ...options }}
       request={async () =>
-        options.map((item: any) => ({
-          label: item[props.fieldProps.fieldNames.label],
-          value: item[props.fieldProps.fieldNames.value],
-        }))
+        options
+          .map((item: any) => ({
+            label: item[props.fieldProps.fieldNames.label],
+            value: item[props.fieldProps.fieldNames.value],
+          }))
+          .map((item: any) => ({
+            label: item.label,
+            value: /^\d+$/.test(item.value) ? Number(item.value) : item.value, //数字字符串转为数字
+          }))
       }
       {...props.formItemProps}
       noStyle
--- src/components/CRUD/Upload.tsx
+++ src/components/CRUD/Upload.tsx
@@ -1,16 +1,16 @@
-import common from '@/services/api/common';
-import tool from '@/services/tool';
-import { ProFormUploadButton } from '@ant-design/pro-components';
-import { useIntl } from '@umijs/max';
-import { Image as AntImage, message as Message } from 'antd';
-import file2md5 from 'file2md5';
-import React from 'react';
+import common from "@/services/api/common";
+import tool from "@/services/tool";
+import { ProFormUploadButton } from "@ant-design/pro-components";
+import { useIntl } from "@umijs/max";
+import { Image as AntImage, message as Message } from "antd";
+import file2md5 from "file2md5";
+import React from "react";
 
 const storageMode = {
-  '1': 'LOCAL',
-  '2': 'OSS',
-  '3': 'COS',
-  '4': 'QINIU',
+  "1": "LOCAL",
+  "2": "OSS",
+  "3": "COS",
+  "4": "QINIU",
 };
 
 /**
@@ -26,46 +26,58 @@ export default (props: {
   const intl = useIntl();
 
   if (props.initialValue) {
-    return props.fieldProps.type === 'image' ? <AntImage src={props.initialValue} /> : null;
+    return props.fieldProps.type === "image" ? (
+      <AntImage
+        src={props.initialValue}
+        style={props.fieldProps?.style ?? undefined}
+      />
+    ) : null;
   } else {
     return (
       <>
         {contextHolder}
         <ProFormUploadButton
-          title={intl.formatMessage({ id: 'component.form.button.upload' })}
+          title={intl.formatMessage({ id: "component.form.button.upload" })}
           name={props.fieldProps.id}
-          max={!props.fieldProps.multiple ? 1 : props.fieldProps?.max ?? undefined}
+          max={
+            !props.fieldProps.multiple ? 1 : props.fieldProps?.max ?? undefined
+          }
           accept={
-            props.fieldProps.type === 'image' && props.fieldProps.accept !== '*'
-              ? '.jpg,jpeg,.gif,.png,.svg,.bpm'
+            props.fieldProps.type === "image" && props.fieldProps.accept !== "*"
+              ? ".jpg,jpeg,.gif,.png,.svg,.bpm"
               : props.fieldProps.accept
           }
           fieldProps={{
-            name: 'file',
-            // fileList: props.fieldProps?.value ?? [],
-            listType: props.fieldProps.type === 'image' ? 'picture-card' : undefined,
+            name: "file",
+            fileList: props.fieldProps?.value ?? [],
+            listType:
+              props.fieldProps.type === "image" ? "picture-card" : undefined,
             customRequest: async ({ file, onSuccess, onError }) => {
               const hash = await file2md5(file as any);
               const formData = new FormData();
               formData.append(props.fieldProps.type, file);
-              formData.append('isChunk', 'false');
-              formData.append('hash', hash);
+              formData.append("isChunk", "false");
+              formData.append("hash", hash);
               if (props.hasWebToken) {
-                formData.append('token', props.hasWebToken);
+                formData.append("token", props.hasWebToken);
               }
               const msg =
-                props.fieldProps.type === 'image'
-                  ? await common.uploadImage(formData, props?.hasWebToken ? true : false)
-                  : await common.uploadFile(formData, props?.hasWebToken ? true : false);
+                props.fieldProps.type === "image"
+                  ? await common.uploadImage(formData, !!props?.hasWebToken)
+                  : await common.uploadFile(formData, !!props?.hasWebToken);
               if (msg.success) {
-                msg.data.url = tool.attachUrl(msg.data.url, storageMode[msg.data.storage_mode]);
-                onSuccess?.(msg);
-                props.fieldProps.onChange?.(
-                  props.fieldProps.onlyData ? msg.data[props.fieldProps.returnType] : msg.data,
+                msg.data.url = tool.attachUrl(
+                  msg.data.url,
+                  storageMode[msg.data.storage_mode]
+                );
+                onSuccess?.(
+                  props.fieldProps.onlyData
+                    ? msg.data[props.fieldProps.returnType]
+                    : msg.data
                 );
               } else {
                 message.error(msg.message);
-                onError?.({ method: 'POST' } as any);
+                onError?.({ method: "POST" } as any);
               }
             },
             onPreview: async (file) => {
@@ -83,8 +95,8 @@ export default (props: {
               imgWindow?.document.write(image.outerHTML);
             },
           }}
+          colProps={{ span: 24 }}
           noStyle
-          {...props.formItemProps}
         />
       </>
     );
